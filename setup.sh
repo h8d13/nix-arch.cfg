ea510b69e1653014b6000927e20a5d015d3678d0
Hade <hadean-eon-dev@proton.me>
2026-07-15 14:14:00 +0200

    create initial script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+pacman -Syy --needed git python terminus-font
+echo "KEYMAP=us" > /etc/vconsole.conf
+echo "FONT=ter-132n" >> /etc/vconsole.conf
+
+git clone https://github.com/h8d13/archinstoo && cd archinstoo
+./DEV --script live
