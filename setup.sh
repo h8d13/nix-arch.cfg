b9c1848dcd977b803ffa8c65564c4f222591cacd
Hade <hadean-eon-dev@proton.me>
2026-07-15 14:26:10 +0200

    Simplify setup script by removing vconsole config
    Removed unnecessary lines for vconsole configuration.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 pacman -Syy || exit 1
 # refresh
-pacman -S --needed --noconfirm git python python-pyparted terminus-font
-echo "KEYMAP=us" > /etc/vconsole.conf
-echo "FONT=ter-132n" >> /etc/vconsole.conf
+pacman -S --needed --noconfirm git python python-pyparted
 
 git clone https://github.com/h8d13/archinstoo && cd archinstoo
 ./DEV --script live
