043c20c1d8c2a8cd7bfee018a6454b034f364fae
Hade <hadean-eon-dev@proton.me>
2026-07-15 14:22:58 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pacman -Syy || exit 1
 # refresh
-pacman -S --needed --noconfirm git python terminus-font
+pacman -S --needed --noconfirm git python python-pyparted terminus-font
 echo "KEYMAP=us" > /etc/vconsole.conf
 echo "FONT=ter-132n" >> /etc/vconsole.conf
 
