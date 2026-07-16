b693ec28132a672eb66172d4335f8d7377897fbd
h8d13 <hadean-eon-dev@proton.me>
2026-07-16 17:18:29 +0200

    misc
    Signed-off-by: h8d13 <hadean-eon-dev@proton.me>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # more simplistic setup
-pacman -S --needed -noconfirm zram-generator git micro
+# usually ive already set root pw at this step
+
+pacman -S --needed -noconfirm zram-generator ufw git micro
 
 echo "[zram0]" > /etc/systemd/zram-generator.conf
 echo "EDITOR=micro" >> /etc/environment
+ufw enable
