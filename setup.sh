f2816c174e600a19d16be7b098e3604dda117f6c
h8d13 <hadean-eon-dev@proton.me>
2026-07-16 17:23:55 +0200

    misc
    Signed-off-by: h8d13 <hadean-eon-dev@proton.me>

--- setup.sh
+++ setup.sh
@@ -1,9 +1,48 @@
 #!/usr/bin/env bash
 # more simplistic setup
 # usually ive already set root pw at this step
+pacman -S --needed --noconfirm zram-generator ufw git micro
 
-pacman -S --needed -noconfirm zram-generator ufw git micro
+# art above, stock \S{PRETTY_NAME} \r (\l) line stays at the bottom.
+# ':::' guard keeps reruns from stacking copies; quoted heredoc
+# because the art itself contains single quotes
+if ! grep -q ':::' /etc/issue; then
+      { cat <<'EOF'
+
+                       .
+                      .:.
+                     .:::.
+                    .:::::.
+                   .:::::::.
+                  .:::::::::.
+                 .::::.:.::::.
+                .::::.:::.::::.
+               .::::.:::::.::::.
+              .::::.:::::::.::::.
+             .::::.:::::::::.::::.
+            .::::.::::.:.::::.::::.
+           .::::.::::'   '::::.::::.
+          .::::.::::'     '::::.::::.
+         .::::.::::'       '::::.::::.
+        .::::.::::'         '::::.::::.
+             .:::'           ':::.
+   ::.      .:::'             ':::.      .::
+    :::.   .:::'               ':::.   .:::
+  ::::::::::::'                 '::::::::::::
+      :::. .:'                   ':. .:::
+       :::..:          .:.          :..:::
+        :::.'          ':'          '.:::
+          ::.                       .::
+         .':::.                   .:::'.
+        .'  :::.                 .:::  '.
+       .'    :::.               .:::    '.
+
+EOF
+      cat /etc/issue; } > /etc/issue.new
+      mv /etc/issue.new /etc/issue
+fi
 
 echo "[zram0]" > /etc/systemd/zram-generator.conf
 echo "EDITOR=micro" >> /etc/environment
 ufw enable
+systemctl enable ufw
