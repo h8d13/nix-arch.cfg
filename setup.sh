1fe62ad2a135cb33c04c20dd92a9ced406e0b005
h8d13 <hadean-eon-dev@proton.me>
2026-07-16 17:38:04 +0200

    git
    implicit

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # more simplistic setup
 # usually ive already set root pw at this step
-pacman -S --needed --noconfirm zram-generator ufw git micro
+pacman -S --needed --noconfirm zram-generator ufw micro
 
 # art above, stock \S{PRETTY_NAME} \r (\l) line stays at the bottom.
 # ':::' guard keeps reruns from stacking copies; quoted heredoc
